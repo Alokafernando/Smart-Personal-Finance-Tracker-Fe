cb75b54822bd23989e4929ffe87c84cc4508a352
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-12-27 15:08:11 +0530

    update

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { useNavigate, useLocation } from "react-router-dom"
-import { Home, BarChartBig as ChartPie, Layers, Users, BarChart3, Tag, Settings, HelpCircle, LogOut, ChevronLeft, ChevronRight, UserCog, } from "lucide-react"
+import { Home, BarChartBig as ChartPie, Layers, Users, BarChart3, Tag, Settings, HelpCircle, LogOut, ChevronLeft, ChevronRight, } from "lucide-react"
 import Swal from "sweetalert2"
 import { getUserDetails } from "../services/auth"
 import { useAuth } from "../context/authContext"
